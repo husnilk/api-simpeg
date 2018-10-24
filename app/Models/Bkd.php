80300cae758985c1bd06121cdafbf8631e58b447
husnilk <husnil.k@gmail.com>
2018-10-24 20:31:32 +0700

    Update bkd

--- app/Models/Bkd.php
+++ app/Models/Bkd.php
@@ -24,15 +24,15 @@ class Bkd extends Model
         return Bkd::where('bkdTahunAkademik', $tahun)
                 ->where('bkdSemester', $semester)
                 ->get([
-                  'bkdId as simpeg_id',
-                  'bkdNama as nama',
-                  'bkdPegId as pegawai_id',
-                  'bkdNIP as nip',
-                  'bkdNoSertifikasi as no_serdos',
-                  'bkdFakultas as fakultas',
-                  'bkdJenis as jenis',
-                  'bkdKesimpulan1 as hasil_1',
-                  'bkdKesimpulan1 as hasil_2'
+                    'bkdId as simpeg_id',
+                    'bkdNama as nama',
+                    'bkdPegId as pegawai_id',
+                    'bkdNIP as nip',
+                    'bkdNoSertifikasi as no_serdos',
+                    'bkdFakultas as fakultas',
+                    'bkdJenis as jenis',
+                    'bkdKesimpulan1 as hasil_1',
+                    'bkdKesimpulan1 as hasil_2'
                 ]);
     }
 
@@ -45,16 +45,16 @@ class Bkd extends Model
                $x->where('satkerId', $unit);
            });
        })->get([
-        'bkdId as simpeg_id',
-        'bkdPegId as pegawai_id',
-        'bkdNama as nama',
-        'bkdNIP as nip',
-        'bkdNoSertifikasi as no_serdos',
-        'bkdFakultas as fakultas',
-        'bkdJenis as jenis',
-        'bkdKesimpulan1 as hasil_1',
-        'bkdKesimpulan1 as hasil_2'
-      ]);
+           'bkdId as simpeg_id',
+           'bkdPegId as pegawai_id',
+           'bkdNama as nama',
+           'bkdNIP as nip',
+           'bkdNoSertifikasi as no_serdos',
+           'bkdFakultas as fakultas',
+           'bkdJenis as jenis',
+           'bkdKesimpulan1 as hasil_1',
+           'bkdKesimpulan1 as hasil_2'
+       ]);
     }
 
     public function byPegawai($tahun, $semester)
@@ -68,21 +68,22 @@ class Bkd extends Model
         $data->no_serdos = $this->bkdNoSertifikasi;
         $data->fakultas = $this->bkdFakultas;
         $data->kinerja = $this->kinerjas()->get([
-        'bkdpendBidBkd as bidang_bkd',
-        'bkdpendJenisKegiatan as kegiatan',
-        'bkdpendMasaPenugasan as masa_penugasan',
-        'bkdpendBebanKerjaSks as sks_beban',
-        'bkdpendKinerjaSks as sks_kinerja',
-        'bkdPendSaranAsesor1 as saran_asesor_1',
-        'bkdPendSaranAsesor2 as saran_asesor_2'
-      ]);
+            'bkdpendBidBkd as bidang_bkd',
+            'bkdpendJenisKegiatan as kegiatan',
+            'bkdpendMasaPenugasan as masa_penugasan',
+            'bkdpendBebanKerjaSks as sks_beban',
+            'bkdpendKinerjaSks as sks_kinerja',
+            'bkdpendRekomendasi as rekomendasi',
+            'bkdPendSaranAsesor1 as saran_asesor_1',
+            'bkdPendSaranAsesor2 as saran_asesor_2'
+        ]);
 
         return $data;
     }
 
     public function kesimpulan()
     {
-        if ($this->bkdKesimpulan1 == 'M' && $this->bkdKesimpulan2 == 'M') {
+        if (strtoupper($this->bkdKesimpulan1) == 'M' && strtoupper($this->bkdKesimpulan2 == 'M')) {
             return 'M';
         } else {
             return 'T';
